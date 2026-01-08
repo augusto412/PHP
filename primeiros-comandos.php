fcd3e0888e9f1e53b03e14a6d64051a259359f4e
augusto neto <augustoneto0213@gmail.com>
2026-01-08 20:48:03 -0300

    if e else

---
+++ primeiros-comandos.php
@@ -0,0 +1,25 @@
+<?php
+$name ="augusto";
+
+//echo $name;
+
+/////////////////////ARRAY/////////////////////
+
+$carros = ['Palio', 'jetta', 'gol'];
+
+//echo $carros [0]
+
+
+
+////////////////////////IF//////////////////////
+
+$email = 'augustoneto@gmial.com';
+$senha = 'senha123';
+
+if($email != '' && $senha != ''){
+    echo 'cadastrado com sucesso';
+}else{
+    echo 'algo deu errado, tente novamente';
+}
+
+?>
