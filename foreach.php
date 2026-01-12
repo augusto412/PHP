5c854d7dc76e61da29fd24d72c9abc38ef505bf6
augusto neto <augustoneto0213@gmail.com>
2026-01-12 20:28:35 -0300

    while e foreach

---
+++ foreach.php
@@ -0,0 +1,20 @@
+<?php
+$compras_da_feira =['Arroz', 'feijão', 'batata', 'farinha'];
+
+foreach($compras_da_feira as $compras_da_feira){
+    echo 'tem que comprar:'.$compras_da_feira. '<br/>' ;
+};
+
+
+$consensionaria = ['Palio', 'Corola', 'Hilux', 'Sw4'];
+
+foreach($consensionaria as $consensionaria){
+    echo 'possui esses carros na consencionaria:'.$consensionaria.'<br/>';
+};
+
+$Pinturas = ['Preto', 'amarelo', 'laranja', 'verde'];
+
+foreach($Pinturas as $Pinturas){
+    echo 'Tintas no estoque:'.$Pinturas.'<br/>';
+}
+?>
--- switch.php
+++ switch.php
@@ -1,17 +1,29 @@
 <?php
-$metodos_de_pagamento = 'boleto';
 
-switch($metodos_de_pagamento){
-    case 'cartão_credito':
-        echo 'fazer integração com cartão de credito';
-        break;
-    case 'boleto':
-        echo 'gerar o envio de boleto por e-mail';
-        break;
+//switch($metodos_de_pagamento){
+    //case 'cartão_credito':
+        //echo 'fazer integração com cartão de credito';
+        //break;
+   // case 'boleto':
+      //  echo 'gerar o envio de boleto por e-mail';
+      //  break;
     
-    default:
-    echo 'por favor, escolha um metodo de pagamento';
-    break;
-}
+   // default:
+    //echo 'por favor, escolha um metodo de pagamento';
+   // break;
+//}
+
+
+////////////////////WHILE//////////////////////
+
+//$quantidade_de_cliente = 20;
+//$boletos_gerados = 1;
+
+//while($boletos_gerados <= $quantidade_de_cliente){
+//echo $boletos_gerados.'-gerando boleto <br/><br/>';
+
+//$boletos_gerados = $boletos_gerados +1;
+//}
+
 
 ?>
