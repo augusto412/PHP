3f11a350d29053fb657a41fc9185e646c04c54f0
augusto neto <augustoneto0213@gmail.com>
2026-01-13 19:58:05 -0300

    function

---
+++ funcoes..php
@@ -0,0 +1,12 @@
+<?php
+
+$primeiro_numero = 10;
+$segundo_numero = 20;
+$operação = "+";
+
+function calcular($primeiro_numero, $segundo_numero){
+    $primeiro_numero + $segundo_numero;
+}
+
+calcular(30, 20);
+?>
