a6357819ed0f7834f23327d1b29aca261cd0d99d
augusto neto <augustoneto0213@gmail.com>
2026-01-19 20:27:09 -0300

    funções nativas php

--- funcoes_nativas.php
+++ funcoes_nativas.php
@@ -2,21 +2,40 @@
 
 //TRTANFORMA NUMEROS NEGATIVOS EM PSOTIVOS
 $nota = '-5';
-$nova_nota = abs ($nota);
+//$nova_nota = abs ($nota);
 //echo $nova_nota;
 
 
 //PARA ARENDONDAR PARA O INTEIRO MAIOR 
 $nota = 5.4;
-$nova_nota = ceil($nota);
+//$nova_nota = ceil($nota);
 //echo $nova_nota
 
 //ARENDODA PARA O INTEIRO MENOR
 $nota = 9.8;
-$nova_nota = floor($nota);
+//$nova_nota = floor($nota);
 //echo $nova_nota
 
 $numero_aleatorio = rand(0, 100);
-echo $numero_aleatorio
+//echo $numero_aleatorio;
 
+////////////////////////FUNÇÕES DE LINGUAGEM///////////////////////////////
+
+//PARA CHIGAMENRTOS
+$mensagem_chat = 'quero que você se ';
+
+if (strpos ($mensagem_chat, 'lasque') != ""){
+    echo 'por favor, retire palavras de odio da mensagem';
+}else{
+    echo ' mensagem enviada com sucesso';
+};
+
+$nome = 'augusto neto';
+$nome = explode(' ', $nome);
+
+
+$nome = ' augusto pedro de sousa neto ';
+$nome = ltrim ($nome);
+$nome = rtrim ($nome);
+echo  strlen($nome);
 ?>
---
+++ funções_array.php
@@ -0,0 +1,13 @@
+<?php
+
+$alimentos = ['arroz', 'Feijão'];
+
+if(is_array ($alimentos)){
+    echo 'isso é um array';
+}else{
+    echo 'isso não é um array'; 
+}
+
+//VAR DUMP//
+var_dump ($alimentos)
+?>
