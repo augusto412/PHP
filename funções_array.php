90529cdc4bf75a4b0154c385f3082ebab5ae09e7
augusto neto <augustoneto0213@gmail.com>
2026-01-21 19:40:23 -0300

    funções nativas

--- funções_array.php
+++ funções_array.php
@@ -1,13 +1,33 @@
 <?php
 
-$alimentos = ['arroz', 'Feijão'];
+$alimentos = ['arroz', 'Feijão', 'batata', 'batata'];
+$produtos= ['papel', 'lapis', 'lapizera'];
 
-if(is_array ($alimentos)){
-    echo 'isso é um array';
-}else{
-    echo 'isso não é um array'; 
-}
+$alimentos [] = 'farinha';
 
-//VAR DUMP//
-var_dump ($alimentos)
+//PARA ADICIONAR NO INICIO DO ARRAY
+array_unshift($alimentos, 'salada');
+
+//PARA NÃO TER DUPLICATAS
+$alimentos = array_unique($alimentos);
+
+print_r($alimentos);
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////
+
+//if(is_array ($alimentos)){
+    //echo 'isso é um array';
+//}else{
+   //echo 'isso não é um array'; 
+//}
+
+//$todosArrays = array_merge($alimentos, $produtos);
+
+//print_r($todosArrays);
+
 ?>
